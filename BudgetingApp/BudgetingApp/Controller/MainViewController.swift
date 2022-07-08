eea629e8e3029b999ebbcccc5b23fc162dbc708e
Olzhas Seiilkhanov <Olzhas.Seiilkhanov@IMAC-3-9.local>
2022-07-08 15:42:49 +0600

    First commit

---
+++ BudgetingApp/BudgetingApp/Controller/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BudgetingApp
+//
+//  Created by Olzhas Seiilkhanov on 08.07.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
